0f9fd5b60558494af5b6b42a741fabd617041e74
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-05-26 23:12:34 +0300

    tabs

--- LImitCharactersInTextField/ContentView.swift
+++ LImitCharactersInTextField/ContentView.swift
@@ -12,13 +12,26 @@ struct ContentView: View {
 
     @ObservedObject var textFieldManager = TextFieldManager()
     
+    @State private var selection = 0
+    
     var body: some View {
-        TextField("Enter something...", text: $textFieldManager.userInput)
-            .padding()
-            .background(Color(red: 239/255, green: 243/255, blue: 244/255))
-            .cornerRadius(10)
-            .padding()
-        
+        TabView(selection: $selection) {
+            TextField("Enter something...", text: $textFieldManager.userInput)
+                .padding()
+                .background(Color(red: 239/255, green: 243/255, blue: 244/255))
+                .cornerRadius(10)
+                .padding()
+            .tabItem {
+                Image(systemName: "1.square")
+                Text("First View")
+            }.tag(0)
+            
+            Text("Second View")
+                .tabItem {
+                    Image(systemName: "2.square")
+                    Text("Second View")
+                }.tag(1)
+        }
     }
 }
 
